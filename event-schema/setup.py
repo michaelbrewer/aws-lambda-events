9dccfcc048d4cff5b7f128b10268b2f2cb3770be
Michael Brewer <michael.brewer@gyft.com>
2022-03-21 16:30:33 -0700

    feat(shareable-events): Closer to being packaged

--- event-schema/setup.py
+++ event-schema/setup.py
@@ -3,14 +3,17 @@ import setuptools
 setuptools.setup(
     name="aws-lambda-publish-shared-event",
     version="1.0",
-    scripts=["./scripts/publish-shared-event"],
+    entry_points={
+        "console_scripts": ["publish-shared-event=main:main"],
+    },
     author="Michael Brewer",
     description="Cli to publish shareable lambda test events.",
     packages=["src"],
     install_requires=[
         "setuptools",
-        "boto3 >= 1.21.22",
+        "boto3 >= 1.21.23",
         "pick >= 1.2.0",
     ],
     python_requires=">=3.7",
+    include_package_data=True,
 )
--- event-schema/src/main.py
+++ event-schema/src/main.py
@@ -14,6 +14,7 @@ registry_name = "lambda-testevent-schemas"
 
 
 def get_main_args_parser() -> argparse.ArgumentParser:
+    """Build the cli argument parser"""
     parser = argparse.ArgumentParser(prog="publish-shared-event", description="List the content of a folder")
     parser.add_argument("-r", dest="region", help="Set AWS Region")
     parser.add_argument("-f", dest="lambda_name", help="Name of the lambda function")
@@ -23,7 +24,8 @@ def get_main_args_parser() -> argparse.ArgumentParser:
     return parser
 
 
-def get_list_of_events() -> List[str]:
+def list_of_test_events() -> List[str]:
+    """Get the list of supported test events"""
     templates = []
     for path, _, files in os.walk(template_root):
         templates.extend(
@@ -34,17 +36,20 @@ def get_list_of_events() -> List[str]:
 
 
 def get_lambda_name(session: boto3.session.Session) -> str:
+    """Prompt to select which lambda event to add a shareable test event."""
     lambda_client = session.client("lambda")
     response = lambda_client.list_functions()
     functions = [f["FunctionName"] for f in response["Functions"]]
     return pick(functions, "Select Lambda function:")[0]
 
 
-def get_event_source(list_of_events: List[str]) -> str:
+def get_test_event(list_of_events: List[str]) -> str:
+    """Prompt to select which test event to use"""
     return pick(list_of_events, "Select Event:")[0]
 
 
 def create_registry_if_not_exists(schemas_client):
+    """Create the event bridge registry for shareable test events"""
     try:
         schemas_client.describe_registry(RegistryName=registry_name)
     except schemas_client.exceptions.NotFoundException:
@@ -58,8 +63,9 @@ def create_registry_if_not_exists(schemas_client):
         )
 
 
-def create_or_update_schema(schemas_client, lambda_name: str, event_source: str):
-    content = generate_schema(event_source)
+def create_or_update_schema(schemas_client, lambda_name: str, test_event: str):
+    """Create or update shareable test event for the specified lambda_name"""
+    content = generate_schema(test_event)
     try:
         schemas_client.create_schema(
             RegistryName="lambda-testevent-schemas",
@@ -79,11 +85,12 @@ def create_or_update_schema(schemas_client, lambda_name: str, event_source: str)
         )
 
 
-def generate_schema(example_file: str) -> str:
-    if exists(example_file):
-        path = Path(example_file)
+def generate_schema(test_event: str) -> str:
+    """Generates an event bridge schema from the test event file"""
+    if exists(test_event):
+        path = Path(test_event)
     else:
-        path = Path(template_root + example_file)
+        path = Path(template_root + test_event)
     example_name = path.name.replace(".json", "")
     event = json.loads(path.read_text())
     schema = {
@@ -106,7 +113,7 @@ def generate_schema(example_file: str) -> str:
 def main():
     args_parser = get_main_args_parser()
     args = args_parser.parse_args()
-    list_of_events = get_list_of_events()
+    list_of_events = list_of_test_events()
 
     if args.list:
         print("List of supported event sources:")
@@ -120,11 +127,11 @@ def main():
 
     session = boto3.session.Session(region_name=args.region)
     lambda_name = args.lambda_name or get_lambda_name(session)
-    event_source = args.event_source or get_event_source(list_of_events)
+    test_event = args.event_source or get_test_event(list_of_events)
     schemas_client = session.client("schemas")
 
     create_registry_if_not_exists(schemas_client)
-    create_or_update_schema(schemas_client, lambda_name, event_source)
+    create_or_update_schema(schemas_client, lambda_name, test_event)
 
 
 if __name__ == "__main__":
