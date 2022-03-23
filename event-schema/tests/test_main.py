74081899e10a07fc536dddfd963484629f1981b9
Michael Brewer <michael.brewer@gyft.com>
2022-03-23 00:28:59 -0700

    test: Add test coverage

--- event-schema/tests/test_main.py
+++ event-schema/tests/test_main.py
@@ -1,10 +1,13 @@
+import importlib
 import json
 import sys
+from pathlib import Path
 from unittest import mock
+from unittest.mock import MagicMock
 
 import pytest
 
-from src.aws_lambda_publish_shared_event.__main__ import generate_new_schema_content, main
+from aws_lambda_publish_shared_event import __main__
 
 SCRIPT_NAME = "foo.py"
 
@@ -12,7 +15,7 @@ SCRIPT_NAME = "foo.py"
 def test_parse_args_help(capsys):
     sys.argv = [SCRIPT_NAME, "--help"]
     with pytest.raises(SystemExit):
-        main()
+        __main__.main()
 
     captured = capsys.readouterr()
     assert captured.out.startswith("usage:")
@@ -21,7 +24,7 @@ def test_parse_args_help(capsys):
 def test_parse_args_list(capsys):
     sys.argv = [SCRIPT_NAME, "--list"]
 
-    main()
+    __main__.main()
 
     captured = capsys.readouterr()
     assert captured.out.startswith("List of supported event sources:")
@@ -30,7 +33,7 @@ def test_parse_args_list(capsys):
 def test_parse_args_filtered(capsys):
     sys.argv = [SCRIPT_NAME, "--filtered-list", "ses/"]
 
-    main()
+    __main__.main()
 
     captured = capsys.readouterr()
     assert captured.out.startswith("Filtered list of supported event sources:")
@@ -38,11 +41,11 @@ def test_parse_args_filtered(capsys):
 
 def test_generate_updated_schema_content():
     sys.argv = [SCRIPT_NAME, "-e", "ses/ses.json", "-f", "name", "-r", "us-east-1"]
-    with mock.patch("src.aws_lambda_publish_shared_event.__main__.get_session") as mock_boto3:
+    with mock.patch("aws_lambda_publish_shared_event.__main__.get_session") as mock_boto3:
         client = mock_boto3.return_value.client
         client.return_value.describe_schema.return_value = {"Content": '{"components": {"examples": {}}}'}
 
-        main()
+        __main__.main()
 
         client.assert_called_with("schemas")
         client.return_value.describe_registry.assert_called_with(RegistryName="lambda-testevent-schemas")
@@ -66,4 +69,47 @@ def test_generate_new_schema_content():
         },
     }
 
-    assert generate_new_schema_content(example_name, event) == json.dumps(expected)
+    assert __main__.generate_new_schema_content(example_name, event) == json.dumps(expected)
+
+
+def test_get_session():
+    assert __main__.get_session("us-east-1") is not None
+
+
+@mock.patch("pick.pick")
+def test_get_test_event(mock_pick: MagicMock):
+    importlib.reload(__main__)
+
+    __main__.get_test_event(["one"])
+
+    mock_pick.assert_called()
+
+
+def test_get_lambda_name():
+    expected = "foo"
+
+    def mock_input(_):
+        return expected
+
+    __main__.input = mock_input
+
+    with mock.patch("boto3.session.Session") as mock_session:
+        assert __main__.get_lambda_name(mock_session) == expected
+        mock_session.client.assert_called_with("lambda")
+        mock_session.client.return_value.list_functions.assert_called()
+
+
+@mock.patch("pick.pick")
+def test_get_lambda_name_list(mock_pick: MagicMock):
+    importlib.reload(__main__)
+
+    with mock.patch("boto3.session.Session") as mock_session:
+        mock_session.client.return_value.list_functions.return_value = {"Functions": [{"FunctionName": "name"}]}
+        assert __main__.get_lambda_name(mock_session) is not None
+
+    mock_pick.assert_called()
+
+
+def test_get_test_event_path():
+    expected = Path(__file__).parent / "test.json"
+    assert __main__.get_test_event_path(str(expected)) == expected
